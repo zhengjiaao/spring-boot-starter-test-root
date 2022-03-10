d001635b3396e63a0fd29a72c71b0e3538281ffe
Zhengjiaao <zhengja@dist.com.cn>
2022-03-10 10:27:08 +0800

    feat(root) 新增-starter-freemarker模块

---
+++ starter-freemarker/README.md
@@ -0,0 +1,5 @@
+# starter-freemarker
+
+> FreeMarker Template Language（FTL） 文件一般保存为 xxx.ftl
+>   - 严格依赖MVC模式，不依赖Servlet容器（不占用JVM内存）
+>   - 支持内建函数
---
+++ starter-freemarker/src/main/java/com/zja/controller/FreemarkerController.java
@@ -0,0 +1,38 @@
+/**
+ * @Company: 上海数慧系统技术有限公司
+ * @Department: 数据中心
+ * @Author: 郑家骜[ào]
+ * @Email: zhengja@dist.com.cn
+ * @Date: 2022-03-10 9:48
+ * @Since:
+ */
+package com.zja.controller;
+
+import com.zja.properties.ServerSetttings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/freemarker")
+public class FreemarkerController {
+
+    @Autowired
+    private ServerSetttings serverSetttings;
+
+    //http://127.0.0.1:8080/freemarker/hello
+    @GetMapping("hello")
+    public String queryById() {
+        return "ftl/index";
+    }
+
+    //http://127.0.0.1:8080/freemarker/hello/v2
+    @RequestMapping("hello/v2")
+    public String hello(Model model) {
+        model.addAttribute("server", serverSetttings);
+        return "ftl/index";
+    }
+
+}

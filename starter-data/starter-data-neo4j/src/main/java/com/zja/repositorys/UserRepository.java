b22fc7ba77bbf91ad7c1c2b843e4fd259a170f9c
Zhengjiaao <zhengja@dist.com.cn>
2022-11-14 17:18:04 +0800

    fix(starter-data-neo4j) 使用cypher语法查询

--- starter-data/starter-data-neo4j/src/main/java/com/zja/repositorys/UserRepository.java
+++ starter-data/starter-data-neo4j/src/main/java/com/zja/repositorys/UserRepository.java
@@ -2,6 +2,10 @@ package com.zja.repositorys;
 
 import com.zja.entity.User;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Company: 上海数慧系统技术有限公司
@@ -12,4 +16,11 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
  * Desc：
  */
 public interface UserRepository extends Neo4jRepository<User, Long> {
+
+    /**
+     * 使用cypher语法查询 自定义查询
+     */
+    @Query("MATCH (n:User) where n.name contains $name RETURN n")
+    List<User> getUserByName(@Param("name") String name);
+
 }
